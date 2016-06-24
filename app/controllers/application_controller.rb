f437e0ccd0a2fae8cf80255d680a87553909bb53
Yannick Schutz <yannick.schutz@gmail.com>
2016-06-24 15:19:23 +0200

    Add Health, boomtown and a before_action for authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,22 @@
+# Base application controller
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :authenticate!, except: [:health, :boomtown]
+
+  def health
+    render json: { status: "OK" }, status: :ok
+  end
+
+  def boomtown
+    raise("Intentional exception from the web app")
+  end
+
+  private
+
+  def authenticate!
+    redirect_to "/auth/github" unless session["user"]
+  end
 end
